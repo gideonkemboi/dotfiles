aca01e224d23d4def7432eed931ebb4ecf6cd9c2
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-10-08 12:06:13 +0300

    Configure find and replace using Spectre

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,4 +1,3 @@
-local lazy = require "lazy"
 local plugins = {
   {
     "Pocco81/auto-save.nvim",
@@ -89,6 +88,45 @@ local plugins = {
   {
     "wakatime/vim-wakatime",
     lazy = false,
+  },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-pack/nvim-spectre", lazy = false },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
 }
 
