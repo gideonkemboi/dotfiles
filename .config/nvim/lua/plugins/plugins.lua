1da172978439ad3518b1b350b9a868a8561a4732
Gideon Kemboi <gedionkemboi18@gmail.com>
2025-04-24 18:27:47 +0300

    feat: Add plugin for session management

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -177,6 +177,18 @@ local plugins = {
       },
     }, -- your configuration
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
 }
 
 return plugins
